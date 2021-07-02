6d1919e48154cb1f567b3ee2631736b7285d94b3
drickert11 <rickert.dustin@gmail.com>
2021-07-01 21:51:25 -0400

    combined the sql scripts for easier usage within docker

--- db/create_database.sql
+++ db/create_database.sql
@@ -1,1 +1,14 @@
-createdb bookstore;
+createdb Bookstore;
+
+use Bookstore;
+DROP TABLE IF EXISTS Books;
+
+CREATE TABLE Books (
+    [ID] SERIAL PRIMARY KEY NOT NULL,
+    [Title] varchar(100) NOT NULL,
+    [Author] varchar(100) NOT NULL,
+    [Publisher] varchar(100) NOT NULL,
+    [PublishDate] date NOT NULL,
+    [Rating] int NOT NULL, -- 1-3
+    [Status] varchar(10) NOT NULL -- (CheckedIn, CheckedOut)
+);
