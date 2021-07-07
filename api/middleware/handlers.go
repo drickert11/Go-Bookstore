272985fd65502553dfd9f0134a401c3d1586bcf5
drickert11 <rickert.dustin@gmail.com>
2021-07-07 05:25:47 -0400

    fixed up handlers

--- api/middleware/handlers.go
+++ api/middleware/handlers.go
@@ -23,21 +23,18 @@ type response struct {
 }
 
 func createConnection() *sql.DB {
-	// load .env file
 	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
-	//open the connection
 	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 
 	if err != nil {
 		panic(err)
 	}
 
-	// check the connection
 	err = db.Ping()
 
 	if err != nil {
@@ -45,13 +42,10 @@ func createConnection() *sql.DB {
 	}
 
 	fmt.Println("Successfully connected!")
-	// return the connection
 	return db
 }
 
 func AddBook(w http.ResponseWriter, r *http.Request) {
-	// set the header to content type x-www-form-urlencoded
-	// Allow all origin to handle cors issue
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
@@ -59,8 +53,7 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 
 	var book models.Book
 
-	//decode body
-	err := json.NewDecoder(r.Body).Decode(&book) //modularize?
+	err := json.NewDecoder(r.Body).Decode(&book)
 
 	if err != nil {
 		log.Fatalf("Unable to decode the request body. %v", err)
@@ -73,19 +66,15 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 		Message: "Book was created seccessfully",
 	}
 
-	//encode and send response
 	json.NewEncoder(w).Encode(response)
-
 }
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	//route variable: should just be id
 	params := mux.Vars(r)
 
-	// convert the id type from string to int
 	id, err := strconv.Atoi(params["id"])
 
 	if err != nil {
@@ -98,7 +87,6 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Unable to get book. %v", err)
 	}
 
-	// send the response
 	json.NewEncoder(w).Encode(book)
 }
 
@@ -122,7 +110,6 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	//retrieve id parameter
 	params := mux.Vars(r)
 
 	id, err := strconv.Atoi(params["id"])
@@ -141,16 +128,13 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 
 	updateCount := updateBook(int64(id), book)
 
-	// format the message string
 	msg := fmt.Sprintf("Book was updated successfully. Rowcount: %v", updateCount)
 
-	// format the response message
 	res := response{
 		ID:      int64(id),
 		Message: msg,
 	}
 
-	// send the response
 	json.NewEncoder(w).Encode(res)
 }
 
@@ -181,15 +165,14 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res)
 }
 
-//------------------------- handler functions ----------------
-// split to separate class?
+// ----------------- Data Access --------------------
 func insertBook(book models.Book) int64 {
 
 	db := createConnection()
 
 	defer db.Close()
 
-	sqlStatement := `InsertBook (title, author, publisher, publishDate, rating, status) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id` //stored proc
+	sqlStatement := `INSERT INTO books (title, author, publisher, publishDate, rating, status) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id` //stored proc
 
 	var id int64
 
@@ -224,7 +207,7 @@ func getBook(id int64) (models.Book, error) {
 	case nil:
 		return book, nil
 	default:
-		log.Fatalf("Unable to scan the row. %v", err)
+		log.Fatalf("Issue with Row scan. %v", err)
 	}
 
 	return book, err
@@ -237,9 +220,8 @@ func getAllBooks() ([]models.Book, error) {
 
 	var books []models.Book
 
-	sqlStatement := `SELECT * FROM Books`
+	sqlStatement := `SELECT * FROM books`
 
-	// execute the sql statement
 	rows, err := db.Query(sqlStatement)
 
 	if err != nil {
@@ -270,7 +252,7 @@ func updateBook(id int64, book models.Book) int64 {
 
 	defer db.Close()
 
-	sqlStatement := `UPDATE Books SET Title=$2, Author=$3, Publisher=$4, PublishDate=$5, Rating=$6, Status=$7 WHERE ID=$1`
+	sqlStatement := `UPDATE books SET title=$2, author=$3, publisher=$4, publishdate=$5, rating=$6, status=$7 WHERE id=$1`
 
 	res, err := db.Exec(sqlStatement, book.ID, book.Title, book.Author, book.Publisher, book.PublishDate, book.Rating, book.Status)
 
@@ -278,7 +260,7 @@ func updateBook(id int64, book models.Book) int64 {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
 
-	rowsAffected, err := res.RowsAffected() //swap to boolean?
+	rowsAffected, err := res.RowsAffected()
 
 	if err != nil {
 		log.Fatalf("Error while checking the affected rows. %v", err)
@@ -295,7 +277,7 @@ func deleteBook(id int64) int64 {
 
 	defer db.Close()
 
-	sqlStatement := `DELETE FROM Books WHERE ID=$1`
+	sqlStatement := `DELETE FROM books WHERE id=$1`
 
 	res, err := db.Exec(sqlStatement, id)
 
